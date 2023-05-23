1de1283b8b66d76d484380843a572b9eaff955d1
Qu1etboy <nonzagreanthai@gmail.com>
2023-05-24 04:24:55 +0700

    chore: use global command on production

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -10,20 +10,27 @@ export default defineEventHandler({
       (command) => command.data
     );
 
-    // Set guild commands
-    const guildId = process.env.GUILD_ID as string;
-
     try {
-      const guild = client.guilds.cache.get(guildId);
-      if (!guild) {
-        throw new Error(`Guild ${guild} not found`);
+      // In development use guild command to get an update on command instantly,
+      // but in production use application command so it can be used on another server
+      if (process.env.NODE_ENV === 'production') {
+        await client.application?.commands.set(commands_data);
+        console.info(
+          `[ready] ${client.commands.size} application commands registered`
+        );
+      } else {
+        const guildId = process.env.GUILD_ID as string;
+        const guild = client.guilds.cache.get(guildId);
+        if (!guild) {
+          throw new Error(`Guild ${guild} not found`);
+        }
+        await guild.commands.set(commands_data);
+        console.info(
+          `[ready] ${client.commands.size} guild commands registered on ${guild.name}`
+        );
       }
-      await guild.commands.set(commands_data);
-      console.info(
-        `[ready] ${client.commands.size} guild commands registered on ${guild.name}`
-      );
     } catch (error) {
-      console.error('[ready] Unable to set guild commands:', error);
+      console.error('[ready] Unable to set commands:', error);
     }
   },
 });
